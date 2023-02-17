3d8a1d7e2e9e509052fe06a405f9b9c6e0b12022
Daniel Higginson <danbobhig@gmail.com>
2023-02-17 15:42:26 +0000

    add NavBarForm component with NavBarChild component

---
+++ my-app/src/components/NavBarForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import css from "./css/NavBarSimple.module.css";
+import NavBarChild from "./NavBarChild";
+
+// always use this constructor format:
+class NavBarForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: true,
+        };
+    }
+
+    handleClick = () => {
+        this.setState((prevState) => ({
+            isLoggedIn: prevState.isLoggedIn ? false: true
+        }), () => console.log(this.state.isLoggedIn))
+    }
+
+    render() {
+        return (
+            <div className={css.NavBar}>
+                <h1>My Gallery</h1>
+                <NavBarChild 
+                    isLoggedIn={this.state.isLoggedIn}
+                    handleClick={this.handleClick}
+                />
+            </div>
+        )
+    }
+}
+
+export default NavBarForm;
