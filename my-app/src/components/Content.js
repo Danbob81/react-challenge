2077d1f6c45fc4894716c3ce6309dc1fd00902b0
Daniel Higginson <danbobhig@gmail.com>
2023-03-11 21:52:35 +0000

    add search feature

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -9,6 +9,7 @@ export class Content extends React.Component {
         super(props);
         this.state = {
             isLoaded: false,
+            posts: [],
         }
     }
 
@@ -16,18 +17,38 @@ export class Content extends React.Component {
         setTimeout(() => {
             this.setState({
                 isLoaded: true,
+                posts: savedPosts
             })
         }, 2000)
     }
-
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        console.log(name)
+        const filteredPosts = savedPosts.filter((post) => {
+            return post.name.toLowerCase().includes(name);
+        })
+        this.setState({
+            posts: filteredPosts
+        })
+    }
     render() {
         return (
             <div className={css.Content}>
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor='searchInput'>Search: </label>
+                        <input 
+                        onChange={(e) => this.handleChange(e)}
+                        type="search" 
+                        id="searchInput"
+                        placeholder="By Author"
+                        />
+                        <h4>posts found {this.state.posts.length}</h4>
+                    </form>
                 </div>
                 <div className={css.SearchResults}>
-                    {this.state.isLoaded ? <PostItem savedPosts={savedPosts} /> : <Loader />}
+                    {this.state.isLoaded ? <PostItem savedPosts={this.state.posts} /> : <Loader />}
                 </div>
             </div>
         )
